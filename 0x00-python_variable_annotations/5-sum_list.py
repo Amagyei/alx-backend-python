fd0bbe5343be1bf6d980e0caa664ded67a94ce0d
amagyei <kwame.na@hotmail.com>
2024-10-14 20:16:07 +0000

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 ''' some code to do some things for alx '''
+from typing import Callable, Iterator, Union, Optional, List
 
 def sum_list(input_list: List[float]) -> float:
     return sum(input_list)
