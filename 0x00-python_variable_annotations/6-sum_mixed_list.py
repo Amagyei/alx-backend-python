9ff604c60e84fa165f5857dc1ef3195c35f635e2
amagyei <kwame.na@hotmail.com>
2024-10-14 20:18:52 +0000

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' some code to do some things for alx '''
+
+
+from typing import Callable, Iterator, Union, Optional, List
+
+
+def sum_mixed_list(mxd_lst: List[int]) -> float:
+    return float(sum(input_list))
